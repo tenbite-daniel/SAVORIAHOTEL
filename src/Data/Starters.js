ba33939c3f11ec1ec7bd35074c50f0767244eb0a
bahati <bahatifidelis2004@gmail.com>
2025-07-11 08:55:26 +0300

    menu,starters and footer page

---
+++ src/Data/Starters.js
@@ -0,0 +1,55 @@
+import TomatoBasil from '../assets/images/TomatoBasil.png';
+import Calamari from '../assets/images/Calamari.png';
+import Trio from '../assets/images/Bruschetta.png';
+import Mushrooms from '../assets/images/Stuffed Mushrooms.png';
+import Cocktail from '../assets/images/Shrimp cocktail.png';
+import Spinach from '../assets/images/Spinachdip.png';
+
+
+
+const starters = [
+    {
+        name: "Tomato Basil Soup",
+        description:"A creamy tomato soup served with fresh basil and garlic bread, perfect for warming your soul." ,
+        price: 8.99,
+        category: "Starters",
+        image: TomatoBasil
+    },
+    {
+        name: "Crispy Clamari",
+        description: "Golden fried squid rings served with zesty marinara sauce and fresh lemon wedges.",
+        price: 12.99,
+        category: "Starters",
+        image: Calamari
+    },
+    {
+        name: "Bruschetta Trio",
+        description: "Three varieties of toasted bread topped with fresh tomatoes, mozzarella, and aromatic herbs.",
+        price: 10.99,
+        category: "Starters",
+        image: Trio
+    },
+    {
+        name: "Stuffed Mussrooms",
+        description: "Button mushrooms filled with herbed cream cheese, breadcrumbs, and baked to perfection.",
+        price: 9.99,
+        category: "Starters",
+        image:  Mushrooms
+    },
+    {
+        name: "Shrimp Cocktail",
+        description: "Chilled jumbo shrimp served with our signature cocktail sauce and fresh lemon.",
+        price: 14.99,
+        category: "Starters",
+        image: Cocktail
+    },
+    {
+        name: "Spinach Artichoke Dip",
+        description: "Chilled jumbo shrimp served with our signature cocktail sauce and fresh lemon.",
+        price: 11.99,
+        category: "Starters",
+        image: Spinach
+    }
+];
+
+export default starters;
